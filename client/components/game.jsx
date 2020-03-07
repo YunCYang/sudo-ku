9251c020a180b7661480e0c73c370060636a341e
YunCYang <ericeorl2013@gmail.com>
2020-03-07 12:40:27 -0800

    finished styling for button panel and grid container

--- client/components/game.jsx
+++ client/components/game.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import createPuzzle from '../util/createPuzzle';
+import Grid from './grid';
 
 const Game = () => {
   // let success = false;
@@ -12,10 +13,61 @@ const Game = () => {
   //   }
   // }
   return (
-    <div className='content'>
-      <div className='grid'></div>
-      <div className='number'></div>
-      <div className='action'></div>
+    <div className='content game-content'>
+      <div className='grid'>
+        <Grid />
+      </div>
+      <div className="panel">
+        <div className='number'>
+          <div className="number__button">
+            <button>1</button>
+          </div>
+          <div className="number__button">
+            <button>2</button>
+          </div>
+          <div className="number__button">
+            <button>3</button>
+          </div>
+          <div className="number__button">
+            <button>4</button>
+          </div>
+          <div className="number__button">
+            <button>5</button>
+          </div>
+          <div className="number__button">
+            <button>6</button>
+          </div>
+          <div className="number__button">
+            <button>7</button>
+          </div>
+          <div className="number__button">
+            <button>8</button>
+          </div>
+          <div className="number__button">
+            <button>9</button>
+          </div>
+          <div className="number__button">
+            <button>X</button>
+          </div>
+        </div>
+        <div className='action'>
+          <div className="action__button">
+            <button>
+              <i className="fas fa-arrow-left"></i>
+            </button>
+          </div>
+          <div className="action__button">
+            <button>
+              <i className="fas fa-pen"></i>
+            </button>
+          </div>
+          <div className="action__button">
+            <button>
+              <i className="fas fa-undo"></i>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
