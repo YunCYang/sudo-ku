394aa911d04c11e8489a6ef9c0871b9934a43ecd
YunCYang <ericeorl2013@gmail.com>
2020-03-08 22:58:24 -0700

    set up file structure to make tomorrow's work easier

--- client/components/gridBlock.jsx
+++ client/components/gridBlock.jsx
@@ -5,16 +5,24 @@ const GridBlock = props => {
   const gridDisplay = () => {
     if (props.isCover) return <span>{props.value}</span>;
     else {
-      if (props.isNote) {
+      if (props.memory[1].length) {
         return null;
       } else {
-        return null;
+        return (
+          <div className='circle'>
+            <span>{props.memory[0] === 0 ? '' : props.memory[0]}</span>
+          </div>
+        );
       }
     }
   };
 
+  const clickHandler = () => {
+    return null;
+  };
+
   return (
-    <div className={`col col-${props.index}`}>
+    <div className={`col col-${props.index}`} onClick={clickHandler}>
       {gridDisplay()}
     </div>
   );
